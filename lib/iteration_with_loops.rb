9f1750921cfb44ed2fab2edc81f36c13644e4040
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-24 05:43:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,14 @@
 def join_nested_strings(src)
+new_string = ""    
+   
+src.each do |src_array|
+  src_array.each do |src_detail|
+        if src_detail.is_a? String
+        new_string << src_detail + " "
+    end
+  end
+end
 
+puts new_string
+end
   
